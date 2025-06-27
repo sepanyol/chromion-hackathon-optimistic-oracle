e850d8bd9617b835ff687fc479dbb9cff4ca8bd5
sepanyol <hi@ceballos.io>
2025-06-27 09:31:13 +0200

    update contracts

--- frontend/src/utils/contracts.ts
+++ frontend/src/utils/contracts.ts
@@ -11,20 +11,20 @@ const contractsByChainId: {
 } = {
   // TODO extend
   [avalancheFuji.id]: {
-    factory: "0x12B33F486a69b568651B51253077D1729fBe6a5d",
-    oracle: "0xF15BB9041EEC4169c32C93E05415e7c718961e10",
+    factory: "0xf1b04336b6AB23DCB4B790CDdBb334E178Ed4af7",
+    oracle: "0x4174c9a43DAcC5CEAAc214aCeeB1fEc22919cBe3",
     usdc: "0x5d7d5a45dc75c27ddda489acd6af468b0a54295c",
   },
   [sepolia.id]: {
-    factory: "0x56f81811dBA27107641b0ddb249246eE4c1A9D67",
+    factory: "0x8De61d3d4785E692Fe7425bBEfF28509221e6958",
     usdc: "0xbb04b1d2cadee0f7954f783e96d47cb59f7a0b7d",
   },
   [arbitrumSepolia.id]: {
-    factory: "0x44d62D55BeDAe8DEAc46180065De0E2eE5FA0349",
+    factory: "0xbDF676AE20BeBFf9E4246A0e808054D9d7749A7b",
     usdc: "0xdb76f7fdaeea53874d8f4795a99ffbc47a53a548",
   },
   [baseSepolia.id]: {
-    factory: "0x3F591F0Ae877bE2b35FD8616439c3E0a2b88f57E",
+    factory: "0x17Abf983927B4EdD8aE222cD261C152C3d85e10f",
     usdc: "0x8cd493fdf0c8f61e0d6f99b9ee5303e1a9ec78f7",
   },
 };
